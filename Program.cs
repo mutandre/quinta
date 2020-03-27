030cd826d716e6ad49902a3baf48df3299dc6c88
Mutu Andre <mutuandre@gmail.com>
2020-03-27 20:22:17 -0300

    Atualz1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace _9
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
